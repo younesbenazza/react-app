6dbdd94d9305c068de950fcd92ee1e4568bd3e3a
younesbenazza <89698370+younesbenazza@users.noreply.github.com>
2024-06-07 17:09:57 +0100

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
 const api = axios.create({
-  baseURL: "http://heythemaimeur.pythonanywhere.com",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use(
